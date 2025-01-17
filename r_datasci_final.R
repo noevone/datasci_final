fae3824b43d014a329065ae231336a7a727b9f9d
Noelle <noevone23@gmail.com>
2025-01-17 13:42:29 -0500

    Add files via upload
    THE R FILE AGAIN SDORRYYYYTTY

--- r_datasci_final.R
+++ r_datasci_final.R
@@ -7,69 +7,74 @@ library(latticeExtra)
 
 glimpse(df)
 
-df$Age <- NULL
-
-
-
-# Heatmap 
-# Libraries
-install.packages('hrbrthemes')
-install.packages('viridis')
-install.packages('plotly')
-library(hrbrthemes)
-library(viridis)
-library(plotly)
-# d3heatmap is not on CRAN yet, but can be found here: https://github.com/talgalili/d3heatmap
-if (!require("devtools")) install.packages("devtools")
-devtools::install_github("talgalili/d3heatmap")
-library(d3heatmap)
+#df$Age <- NULL
 
 # Load data
 df <- read_csv("pcos_data.csv")
-colnames(df) <- gsub("\\.", " ", colnames(df))
+
+all_yes <- df %>%
+  filter(PCOS == 1)
+
+all_no <- df %>%
+  filter(PCOS == 0)
+
+pie <- ggplot(all_yes, aes(x = "", y=Hirsutism,fill = factor(Hirsutism))) + 
+  geom_bar(width = 1, stat = "identity") +
+  theme(axis.line = element_blank(), 
+        plot.title = element_text(hjust=0.5)) + 
+  labs(fill="Hirsutism", 
+       x=NULL, 
+       y=NULL, 
+       title="sfsdfsd", 
+       caption="Source: Kaggle")
+
+pie + coord_polar(theta = "y", start=0)
 
 
 
 
-# Matrix format
-dff <- df%>% select(-starts_with('A'))
-df1 <- as.matrix(dff)
+ggplot(all_yes, aes(x = ))
 
-# Heatmap
-#d3heatmap(mat, scale="column", dendrogram = "none", width="800px", height="80Opx", colors = "Blues")
-install.packages('heatmaply')
-library(heatmaply)
-p <- heatmaply(df1,
-               dendrogram = "none",
-               xlab = "", ylab = "",
-               main = "",
-               scale = "column",
-               margins = c(60,100,40,20),
-               grid_color = "white",
-               grid_width = 0.00001,
-               titleX = FALSE,
-               hide_colorbar = TRUE,
-               branches_lwd = 0.1,
-               fontsize_row = 5, fontsize_col = 5,
-               labCol = colnames(df1),
-               labRow = rownames(df1),
-               heatmap_layers = theme(axis.line = element_blank())
-)
-p <- heatmaply(df1,
-               #dendrogram = "row",
-               xlab = "", ylab = "",
-               main = "",
-               scale = "column",
-               margins = c(60,100,40,20),
-               grid_color = "white",
-               grid_width = 0.00001,
-               titleX = FALSE,
-               hide_colorbar = TRUE,
-               branches_lwd = 0.1,
-               label_names = c("Country", "Feature:", "Value"),
-               fontsize_row = 5, fontsize_col = 5,
-               labCol = colnames(df1),
-               labRow = rownames(df1),
-               heatmap_layers = theme(axis.line=element_blank())
-)
-print(p)
+
+ggplot(all_yes, aes(x=Diet_Fried_Food)) +
+  geom_histogram(
+    binwidth = 1, 
+    fill = "green") + 
+  labs(
+    title = 'Fried Food ',
+    x = 'Days of the Week', 
+    y = 'Count of people'
+  )
+
+ggplot(all_no, aes(x= Diet_Fried_Food)) +
+  geom_histogram(
+    binwidth = 1, 
+    fill = "red") + 
+  labs(
+    title = 'Fried Food',
+    x = 'Days of the Week', 
+    y = 'Count of people'
+  )
+
+#Sweets 
+
+
+ggplot(all_yes, aes(x=Diet_Sweets)) +
+  geom_histogram(
+    binwidth = 1, 
+    fill = "green") + 
+  labs(
+    title = 'Sweet Food ',
+    x = 'Days of the Week', 
+    y = 'Count of people'
+  )
+
+ggplot(all_no, aes(x= Diet_Sweets)) +
+  geom_histogram(
+    binwidth = 1, 
+    fill = "red") + 
+  labs(
+    title = 'Sweets Food',
+    x = 'Days of the Week', 
+    y = 'Count of people'
+  )
