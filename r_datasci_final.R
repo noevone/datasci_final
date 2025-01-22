928efd350def4f322f588a5c77c6bc2bc88865af
Noelle <noevone23@gmail.com>
2025-01-22 12:36:18 -0500

    Add files via upload

--- r_datasci_final.R
+++ r_datasci_final.R
@@ -18,14 +18,14 @@ all_yes <- df %>%
 all_no <- df %>%
   filter(PCOS == 0)
 
-pie <- ggplot(all_yes, aes(x = "", y=Hirsutism,fill = factor(Hirsutism))) + 
+pie <- ggplot(all_yes, aes(x = "", y=Stressed,fill = factor(Stressed))) + 
   geom_bar(width = 1, stat = "identity") +
   theme(axis.line = element_blank(), 
         plot.title = element_text(hjust=0.5)) + 
-  labs(fill="Hirsutism", 
+  labs(fill="Stress", 
        x=NULL, 
        y=NULL, 
-       title="sfsdfsd", 
+       title="Stress (PCOS)", 
        caption="Source: Kaggle")
 
 pie + coord_polar(theta = "y", start=0)
@@ -62,19 +62,112 @@ ggplot(all_no, aes(x= Diet_Fried_Food)) +
 ggplot(all_yes, aes(x=Diet_Sweets)) +
   geom_histogram(
     binwidth = 1, 
-    fill = "green") + 
+    fill = "darkolivegreen3") + 
   labs(
-    title = 'Sweet Food ',
+    title = 'Sweet Food (PCOS)',
     x = 'Days of the Week', 
     y = 'Count of people'
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle=0, 
+      vjust=0.6,
+      color = "pink4"),
+    plot.title = element_text(color = 'pink4'),
+    panel.background = element_rect((fill = "antiquewhite"))
   )
 
 ggplot(all_no, aes(x= Diet_Sweets)) +
   geom_histogram(
     binwidth = 1, 
-    fill = "red") + 
+    fill = "indianred1") + 
   labs(
-    title = 'Sweets Food',
+    title = 'Sweet Food (No PCOS)',
     x = 'Days of the Week', 
     y = 'Count of people'
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle=0, 
+      vjust=0.6,
+      color = "pink4"),
+    plot.title = element_text(color = 'pink4'),
+    panel.background = element_rect((fill = "antiquewhite"))
+  )
+
+#stress
+
+ggplot(all_yes, aes(x=Stressed)) +
+  geom_histogram(
+    binwidth = 1, 
+    fill = "darkolivegreen3") + 
+  labs(
+    title = 'Stress (PCOS)',
+    x = 'No vs Yes', 
+    y = 'Count of people'
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle=0, 
+      vjust=0.6,
+      color = "pink4"),
+    plot.title = element_text(color = 'pink4'),
+    panel.background = element_rect((fill = "antiquewhite"))
+  )
+
+ggplot(all_no, aes(x= Stressed)) +
+  geom_histogram(
+    binwidth = 1, 
+    fill = "indianred1") + 
+  labs(
+    title = 'Stress (No PCOS)',
+    x = 'No vs Yes', 
+    y = 'Count of people'
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle=0, 
+      vjust=0.6,
+      color = "pink4"),
+    plot.title = element_text(color = 'pink4'),
+    panel.background = element_rect((fill = "antiquewhite"))
+  )
+
+# Hyperandrogenism
+
+ggplot(all_yes, aes(x=Hyperandrogenism)) +
+  geom_histogram(
+    binwidth = 1, 
+    fill = "darkolivegreen3") + 
+  labs(
+    title = 'Hyperandrogenism in People with PCOS',
+    x = 'No vs Yes', 
+    y = 'Count of people'
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle=0, 
+      vjust=0.6,
+      color = "pink4"),
+    plot.title = element_text(color = 'pink4'),
+    panel.background = element_rect((fill = "antiquewhite"))
+  )
+
+#Hormonal imbalance 
+ggplot(all_yes, aes(x=Hormonal_Imbalance)) +
+  geom_histogram(
+    binwidth = 1, 
+    fill = "darkolivegreen3") + 
+  labs(
+    title = 'Hormonal Imbalances in People with PCOS',
+    x = 'No vs Yes', 
+    y = 'Count of people'
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle=0, 
+      vjust=0.6,
+      color = "pink4"),
+    plot.title = element_text(color = 'pink4'),
+    panel.background = element_rect((fill = "antiquewhite"))
   )
