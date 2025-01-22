ab5d1f5973afe93d5e57a115c630da1319934c00
Ryann69 <princessr011107@gmail.com>
2025-01-22 13:25:49 -0500

    FINAL R GRAPHS
    FINAL R GRAPHS

--- r_datasci_final.R
+++ r_datasci_final.R
@@ -18,7 +18,7 @@ all_yes <- df %>%
 all_no <- df %>%
   filter(PCOS == 0)
 
-pie <- ggplot(all_yes, aes(x = "", y=Stressed,fill = factor(Stressed))) + 
+pie <- ggplot(df, aes(x = "", y=Stressed,fill = factor(PCOS))) + 
   geom_bar(width = 1, stat = "identity") +
   theme(axis.line = element_blank(), 
         plot.title = element_text(hjust=0.5)) + 
@@ -26,7 +26,14 @@ pie <- ggplot(all_yes, aes(x = "", y=Stressed,fill = factor(Stressed))) +
        x=NULL, 
        y=NULL, 
        title="Stress (PCOS)", 
-       caption="Source: Kaggle")
+       caption="Source: Kaggle") +
+  scale_fill_manual(
+    values = c(0 = 'palevioletred', 1 = 'lightskyblue')) +
+  theme(
+    plot.background = element_rect('thistle'),
+    panel.background = element_rect('honeydew')
+  )
+
 
 pie + coord_polar(theta = "y", start=0)
 
@@ -36,138 +43,110 @@ pie + coord_polar(theta = "y", start=0)
 ggplot(all_yes, aes(x = ))
 
 
-ggplot(all_yes, aes(x=Diet_Fried_Food)) +
-  geom_histogram(
-    binwidth = 1, 
-    fill = "green") + 
+ggplot(df, aes(x=Diet_Fried_Food)) +
+  geom_histogram(aes(fill=factor(PCOS, labels = c('False','True'))),
+    binwidth = 1) + 
   labs(
     title = 'Fried Food ',
     x = 'Days of the Week', 
-    y = 'Count of people'
+    y = 'Count of people', fill='PCOS',
+    caption='Source: PCOS Dataset'
+  ) +
+  scale_fill_manual(
+    values = c('False' = 'palevioletred', 'True' = 'lightskyblue')) +
+  theme(
+    plot.background = element_rect('thistle'),
+    panel.background = element_rect('honeydew')
   )
 
-ggplot(all_no, aes(x= Diet_Fried_Food)) +
-  geom_histogram(
-    binwidth = 1, 
-    fill = "red") + 
-  labs(
-    title = 'Fried Food',
-    x = 'Days of the Week', 
-    y = 'Count of people'
-  )
 
 #Sweets 
 
 
-ggplot(all_yes, aes(x=Diet_Sweets)) +
-  geom_histogram(
-    binwidth = 1, 
-    fill = "darkolivegreen3") + 
+ggplot(df, aes(x=Diet_Sweets)) +
+  geom_histogram(aes(fill=factor(PCOS, labels = c('False','True'))),
+    binwidth = 1) + 
   labs(
-    title = 'Sweet Food (PCOS)',
+    title = 'Sweet Food',
     x = 'Days of the Week', 
-    y = 'Count of people'
+    y = 'Count of people', fill='PCOS',
+    caption='Soruce: PCOS Dataset'
   ) +
+  scale_fill_manual(
+    values = c('False' = 'palevioletred', 'True' = 'lightskyblue')) +
   theme(
-    axis.text.x = element_text(
-      angle=0, 
-      vjust=0.6,
-      color = "pink4"),
-    plot.title = element_text(color = 'pink4'),
-    panel.background = element_rect((fill = "antiquewhite"))
-  )
-
-ggplot(all_no, aes(x= Diet_Sweets)) +
-  geom_histogram(
-    binwidth = 1, 
-    fill = "indianred1") + 
-  labs(
-    title = 'Sweet Food (No PCOS)',
-    x = 'Days of the Week', 
-    y = 'Count of people'
-  ) +
-  theme(
-    axis.text.x = element_text(
-      angle=0, 
-      vjust=0.6,
-      color = "pink4"),
-    plot.title = element_text(color = 'pink4'),
-    panel.background = element_rect((fill = "antiquewhite"))
+    plot.background = element_rect('thistle'),
+    panel.background = element_rect('honeydew')
   )
 
 #stress
 
-ggplot(all_yes, aes(x=Stressed)) +
-  geom_histogram(
-    binwidth = 1, 
-    fill = "darkolivegreen3") + 
+ggplot(df, aes(x=Stressed)) +
+  geom_histogram(aes(fill=factor(PCOS, labels = c('False','True'))),
+    binwidth = 1) + 
   labs(
-    title = 'Stress (PCOS)',
+    title = 'Stress',
     x = 'No vs Yes', 
-    y = 'Count of people'
+    y = 'Count of people', fill='PCOS',
+    caption='Source: PCOS Dataset'
   ) +
+  scale_fill_manual(
+    values = c('False' = 'palevioletred', 'True' = 'lightskyblue')) +
   theme(
-    axis.text.x = element_text(
-      angle=0, 
-      vjust=0.6,
-      color = "pink4"),
-    plot.title = element_text(color = 'pink4'),
-    panel.background = element_rect((fill = "antiquewhite"))
+    plot.background = element_rect('thistle'),
+    panel.background = element_rect('honeydew')
   )
 
-ggplot(all_no, aes(x= Stressed)) +
-  geom_histogram(
-    binwidth = 1, 
-    fill = "indianred1") + 
-  labs(
-    title = 'Stress (No PCOS)',
-    x = 'No vs Yes', 
-    y = 'Count of people'
-  ) +
-  theme(
-    axis.text.x = element_text(
-      angle=0, 
-      vjust=0.6,
-      color = "pink4"),
-    plot.title = element_text(color = 'pink4'),
-    panel.background = element_rect((fill = "antiquewhite"))
-  )
 
 # Hyperandrogenism
 
-ggplot(all_yes, aes(x=Hyperandrogenism)) +
-  geom_histogram(
-    binwidth = 1, 
-    fill = "darkolivegreen3") + 
+ggplot(df, aes(x=Hyperandrogenism)) +
+  geom_histogram(aes(fill=factor(PCOS, labels = c('False','True'))),
+    binwidth = 1) + 
   labs(
     title = 'Hyperandrogenism in People with PCOS',
     x = 'No vs Yes', 
-    y = 'Count of people'
+    y = 'Count of people', fill='PCOS',
+    caption='Source: PCOS Dataset'
   ) +
+  scale_fill_manual(
+    values = c('False' = 'palevioletred', 'True' = 'lightskyblue')) +
   theme(
-    axis.text.x = element_text(
-      angle=0, 
-      vjust=0.6,
-      color = "pink4"),
-    plot.title = element_text(color = 'pink4'),
-    panel.background = element_rect((fill = "antiquewhite"))
+    plot.background = element_rect('thistle'),
+    panel.background = element_rect('honeydew')
   )
 
 #Hormonal imbalance 
-ggplot(all_yes, aes(x=Hormonal_Imbalance)) +
-  geom_histogram(
-    binwidth = 1, 
-    fill = "darkolivegreen3") + 
+ggplot(df, aes(x=Hormonal_Imbalance)) +
+  geom_histogram(aes(fill=factor(PCOS, labels = c('False','True'))),
+    binwidth = 1) + 
   labs(
     title = 'Hormonal Imbalances in People with PCOS',
     x = 'No vs Yes', 
-    y = 'Count of people'
+    y = 'Count of people', fill='PCOS',
+    caption='Source: PCOS Dataset'
   ) +
+  scale_fill_manual(
+    values = c('False' = 'palevioletred', 'True' = 'lightskyblue')) +
   theme(
-    axis.text.x = element_text(
-      angle=0, 
-      vjust=0.6,
-      color = "pink4"),
-    plot.title = element_text(color = 'pink4'),
-    panel.background = element_rect((fill = "antiquewhite"))
+    plot.background = element_rect('thistle'),
+    panel.background = element_rect('honeydew')
   )
+
+#Marital Status
+ggplot(df, aes(x=Marital_Status)) +
+  geom_histogram(aes(fill=factor(PCOS, labels = c('False','True'))),
+                 binwidth = 1) + 
+  labs(
+    title = 'Marital Status in People with PCOS',
+    x = 'No vs Yes', 
+    y = 'Count of people', fill='PCOS',
+    caption='Source: PCOS Dataset'
+  ) +
+  scale_fill_manual(
+    values = c('False' = 'palevioletred', 'True' = 'lightskyblue')) +
+  theme(
+    plot.background = element_rect('thistle'),
+    panel.background = element_rect('honeydew')
+  )
+
