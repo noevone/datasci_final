d16514c2a287f252614682b4dad692fb377c24c0
Noelle <noevone23@gmail.com>
2025-01-22 13:12:29 -0500

    Add the final version of the pre-processing graphs

--- FinalThing.R
+++ FinalThing.R
@@ -7,35 +7,59 @@ df <- read_csv("pcos_data.csv")
 glimpse(df)
 
 g <- ggplot(df, aes(Family_History_PCOS))
-g + geom_density(aes(fill=factor(PCOS)), alpha=0.5) + 
-  labs(title="PCOS by Family History of PCOS", 
-       subtitle="🦄💖🤠",
+g + geom_density(aes(fill=factor(PCOS, labels = c("False", "True"))), alpha=0.5) + 
+  labs(title="PCOS by Family History of PCOS",
        caption="Source: PCOS Dataset",
        x="PCOS", y="Density",
-       fill="Family History of PCOS")
+       fill="Family History of PCOS") +
+  scale_fill_manual(
+    values = c("False" = "palevioletred", 'True' = 'lightskyblue')) + 
+  theme(
+    plot.background = element_rect('thistle'),
+    panel.background = element_rect('honeydew')
+  )
+ 
 
 g <- ggplot(df, aes(Weight_kg)) + scale_fill_brewer(palette = "Spectral")
-g + geom_histogram(aes(fill=as.factor(PCOS)), 
+g + geom_histogram(aes(fill=factor(PCOS, labels = c("False", "True"))), 
                    bins=35, col = "black",
                    size=.1) +   # change number of bins
   labs(title="PCOS by Weight", 
-       subtitle="🦄💖🤠", 
        caption="Source: PCOS Dataset",
-       x="Weight", y="Count", fill="PCOS")
+       x="Weight", y="Count", fill="PCOS") +
+  scale_fill_manual(
+    values = c("False" = "palevioletred", 'True' = 'lightskyblue')) + 
+  theme(
+    plot.background = element_rect('thistle'),
+    panel.background = element_rect('honeydew')
+  )
+
 
 g <- ggplot(df, aes(Height_ft)) + scale_fill_brewer(palette = "Spectral")
-g + geom_histogram(aes(fill=as.factor(PCOS)), 
+g + geom_histogram(aes(fill=factor(PCOS, labels = c("False", "True"))), 
                    bins=25, col = "black",
                    size=.1) +   # change number of bins
   labs(title="PCOS by Height", 
-       subtitle="🦄💖🤠", 
        caption="Source: PCOS Dataset",
-       x="Height", y="Count", fill="PCOS")
+       x="Height", y="Count", fill="PCOS") + 
+  scale_fill_manual(
+    values = c("False" = "palevioletred", 'True' = 'lightskyblue')) + 
+  theme(
+    plot.background = element_rect('thistle'),
+    panel.background = element_rect('honeydew')
+  )
+
 
 g <- ggplot(df, aes(Menstrual_Irregularity))
-g + geom_density(aes(fill=factor(PCOS)), alpha=0.5) + 
-  labs(title="PCOS by Family History of PCOS", 
-       subtitle="🦄💖🤠",
+g + geom_density(aes(fill=factor(PCOS, labels = c("False", "True"))), alpha=0.5) + 
+  labs(title="PCOS by Menstrual Irregularity", 
        caption="Source: PCOS Dataset",
        x="PCOS", y="Density",
-       fill="Menstrual Irregularity")
+       fill="Menstrual Irregularity") +
+  scale_fill_manual(
+    values = c("False" = "palevioletred", 'True' = 'lightskyblue')) + 
+  theme(
+    plot.background = element_rect('thistle'),
+    panel.background = element_rect('honeydew')
+  )
+
